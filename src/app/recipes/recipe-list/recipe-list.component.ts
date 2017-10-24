5efe0028adcc4b081d175709e3b8808d77c29a71
BrightonNgema <brightonnkosana@gmail.com>
2017-10-24 14:32:01 +0200

    Add option to view clicked recipe and show details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,14 +7,20 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+@Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[]  = [
     new Recipe ('Test', 'This is a test', 'http://www.seriouseats.com/images/2016/12/20161201-crispy-roast-potatoes-28.jpg'),
-    new Recipe ('Test', 'This is a test', 'http://www.seriouseats.com/images/2016/12/20161201-crispy-roast-potatoes-28.jpg'),
-    new Recipe ('Test', 'This is a test', 'http://www.seriouseats.com/images/2016/12/20161201-crispy-roast-potatoes-28.jpg')
+    new Recipe ('Test2', 'This is a test', 'https://cdn2.tmbi.com/TOH/Images/Photos/37/1200x1200/exps8513_FM153592D03_24_3b.jpg'),
+    new Recipe ('Test3', 'This is a test', 'https://www.tgifridays.com/content/dam/www/bb/dist/img/mac_cheese_burger.jpg')
   ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe : Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
